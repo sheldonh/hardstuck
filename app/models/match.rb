e70da83ef236083e80fb95e0e235baf45a744fa0
Sheldon Hearn <sheldonh@starjuice.net>
2021-04-26 10:37:48 +0000

    Resolve feature envy in Match

--- app/models/match.rb
+++ app/models/match.rb
@@ -14,43 +14,17 @@ class Match < ApplicationRecord
   # TODO maybe optimize: grab all the members from highest ranked through lowest ranked, adjust ranks in memory, then do all the saves (with deferred constraints?)
   def apply_ranking_changes
     transaction do
-      Match.connection.execute "SET CONSTRAINTS members_current_rank DEFERRED"
-
       if draw?
         if !lower_ranked_member.rank_adjacent?(higher_ranked_member)
-          uprank(lower_ranked_member)
+          Member.uprank(lower_ranked_member)
         end
       elsif losing_member.outranks?(winning_member)
         rank_difference = (winning_member.current_rank - losing_member.current_rank).abs
         uprank_earned = rank_difference / 2
-        downrank(losing_member)
-        uprank(winning_member, uprank_earned)
+        Member.downrank(losing_member)
+        Member.uprank(winning_member, uprank_earned)
       end
     end
-  end
-
-  private
-
-  # TODO resolve feature envy: move method to Member
-  def downrank(member, by = 1)
-    by.times do
-      member.downrank
-      displace_member = Member.where(current_rank: member.current_rank).first
-      displace_member.uprank
-      displace_member.save!
-    end
-    member.save!
-  end
-
-  # TODO resolve feature envy: move method to Member
-  def uprank(member, by = 1)
-    by.times do
-      member.uprank
-      displace_member = Member.where(current_rank: member.current_rank).first
-      displace_member.downrank
-      displace_member.save!
-    end
-    member.save!
   end
 
 end
